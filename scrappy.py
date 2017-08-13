fc50f20bba63d2e9275f1a18de44aca92d55e046
teimurjan <teymurgg321@gmail.com>
2017-08-13 19:06:18 +0600

    Edited message about checking currency

--- scrappy.py
+++ scrappy.py
@@ -19,6 +19,7 @@ class CurrencyScrappy:
   def check_currencies(self, bot: YobitBot):
     i = 0
     for currency_name in self.currencies_names_:
+      i += 1
       logging.info(get_checking_currency_msg(currency_name, i))
       page_source = html.fromstring(requests.get(get_currency_url(currency_name)).text)
       value = page_source.xpath(CURRENCY_VALUE_XPATH)[0]
