c593e894c791bda18aac7b7f8d281a63a53ce44f
teimurjan <teymurgg321@gmail.com>
2017-08-13 19:01:46 +0600

    Added message about checking currency

--- messages.py
+++ messages.py
@@ -2,6 +2,10 @@ def get_grabbed_currencies_amount_msg(amount):
   return 'Grabbed %s currencies' % amount
 
 
+def get_checking_currency_msg(currency_name, currency_index):
+  return 'Checking %s at %s position' % (currency_name, currency_index + 1)
+
+
 def get_value_raised_msg(currency_name, prev_value, next_value):
   return 'Value for %s raised from %s BTC to %s BTC' % (currency_name, prev_value, next_value)
 
--- scrappy.py
+++ scrappy.py
@@ -2,7 +2,8 @@ from lxml import html
 import requests
 from bot import YobitBot
 from constants import get_currency_url, BASE_URL, CURRENCIES_NAMES_XPATH, CURRENCY_VALUE_XPATH, format_currency_value
-from messages import get_value_raised_msg, get_invalid_value_msg, get_grabbed_currencies_amount_msg
+from messages import get_value_raised_msg, get_invalid_value_msg, get_grabbed_currencies_amount_msg, \
+  get_checking_currency_msg
 import logging
 
 
@@ -16,7 +17,9 @@ class CurrencyScrappy:
     logging.info(get_grabbed_currencies_amount_msg(len(self.currencies_names_)))
 
   def check_currencies(self, bot: YobitBot):
+    i = 0
     for currency_name in self.currencies_names_:
+      logging.info(get_checking_currency_msg(currency_name, i))
       page_source = html.fromstring(requests.get(get_currency_url(currency_name)).text)
       value = page_source.xpath(CURRENCY_VALUE_XPATH)[0]
       formatted_value = self._format_value(currency_name, value)
@@ -31,7 +34,7 @@ class CurrencyScrappy:
 
   def _did_value_raised(self, prev_value, next_value):
     raise_bound = 1
-    if prev_value - next_value >= raise_bound:
+    if next_value - prev_value >= raise_bound:
       return True
     return False
 
