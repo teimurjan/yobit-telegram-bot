c231dd755b3626a753128540df7b513b60797954
Teimur Gasanov <teymurgg321@gmail.com>
2018-03-05 14:32:20 +0600

    Add passing port

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from api_observer.api_observer import ApiObserver
 from bot.bot import YobitBot
-from settings import BOT_TOKEN
+from settings import BOT_TOKEN, PORT
 from models import User, IgnoredCurrency
 from utils import setup_logger
 
@@ -18,7 +18,7 @@ if __name__ == '__main__':
   api_observer_logger = setup_logger(ApiObserver.__name__)
   bot_logger = setup_logger(YobitBot.__name__)
 
-  bot = YobitBot(BOT_TOKEN, bot_logger)
+  bot = YobitBot(BOT_TOKEN, bot_logger, PORT)
   bot.start()
 
   observer = ApiObserver(bot, api_observer_logger)
